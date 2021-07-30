3faf0c25b7b114dd2e78cc0b9a36f6e662a225a9
Roshan John <63011948+roshatron2@users.noreply.github.com>
2021-07-30 20:24:33 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (29.05%) - LeetHub

--- word-break-ii/word-break-ii.cpp
+++ word-break-ii/word-break-ii.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-    vector<string> res;
-    unordered_map<string,string> map;
-    string dfs(string s,string comb, vector<string> words){
+    unordered_map<string,vector<string>> memo;
+    vector<string> dfs(string s,vector<string> words){
+        if(memo[s].size() != 0)
+            return memo[s];
+        vector<string> res;
         if(s == ""){
-            comb.pop_back();
-            res.push_back(comb);
-            return comb;    
+            res.push_back("");
+            return res;     
         }
         for(auto word : words){
             int idx = s.find(word);
             if(idx == 0){
                 string suffix = s.substr(word.size());
-                dfs(suffix,comb + word + " ",words);
+                vector<string> ways = dfs(suffix,words);
+                for(auto way : ways){
+                    res.push_back(word + (!way.empty() ? " " : "") + way);
+                }
             }
         }
-        return "";
+        memo[s] = res;
+        return memo[s];
     }
     vector<string> wordBreak(string s, vector<string>& wordDict) {
-        dfs(s,"",wordDict);
-        return res;
+        return dfs(s,wordDict);
     }
 };
