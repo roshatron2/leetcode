5573e9702a041a4817a4a024b8c3e7aae9677ef5
Roshan John <63011948+roshatron2@users.noreply.github.com>
2021-08-21 15:09:41 +0530

    Time: 16 ms (88.69%), Space: 14.7 MB (31.77%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -4,7 +4,7 @@ public:
         sort(intervals.begin(),intervals.end());
         vector<vector<int>> merged;
         for(auto interval : intervals){
-            if(merged.empty() || merged.back()[1] < interval[0]){
+            if(merged.empty() or merged.back()[1] < interval[0]){
                 merged.push_back(interval);
             } else {
                 merged.back()[1] = max(merged.back()[1],interval[1]);
