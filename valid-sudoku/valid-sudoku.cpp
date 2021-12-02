4e2db2d8dcbc0cddb084f6ebb9d2cb60b6b07b5f
Roshan John <63011948+roshatron2@users.noreply.github.com>
2021-12-02 21:38:10 +0530

    Time: 24 ms (61.50%), Space: 17.8 MB (97.14%) - LeetHub

--- valid-sudoku/valid-sudoku.cpp
+++ valid-sudoku/valid-sudoku.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    //https://leetcode.com/problems/valid-sudoku/discuss/15616/My-12-lines-CC%2B%2B-solution-with-1-time-traversal-and-9x9x3-memory
     bool isValidSudoku(vector<vector<char>>& board) {
-        int rows[9][10] = {0};
-        int cols[9][10] = {0};
-        int blocks[3][3][10] = {0};
-        
+        int map[3][3][10] = {0};
         for(int i = 0; i < board.size(); i++){
-            for(int j = 0; j < board[0].size(); j++){
-                if(board[i][j] != '.'){
-                    int num = board[i][j] - '0';
-                    if(rows[i][num] or cols[j][num] or blocks[i/3][j/3][num])
+            for(int j = 0; j < board.size(); j++){
+                char cur = board[i][j];
+                if(cur == '.')
+                    continue;
+                for(int k = j + 1; k < 9; k++){
+                    if(board[i][k] == cur)
                         return false;
-                    rows[i][num] = 1;
-                    cols[j][num] = 1;
-                    blocks[i/3][j/3][num] = 1;
                 }
+                for(int k = i + 1; k < 9; k++){
+                    if(board[k][j] == cur){
+                        return false;
+                    }
+                }
+                if(map[i/3][j/3][cur - '0'])
+                    return false;
+                map[i/3][j/3][cur - '0']++;
             }
         }
         return true;
