b33d89efb19c1bff03b7fa668d91996c25439260
Roshan John <63011948+roshatron2@users.noreply.github.com>
2021-12-10 22:56:43 +0530

    Time: 4 ms (70.71%), Space: 7.8 MB (56.75%) - LeetHub

--- permutations/permutations.cpp
+++ permutations/permutations.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    void backtrack(int begin,vector<int> nums){
-        if(begin == nums.size()){
+    void dfs(vector<int> &nums,int begin){
+        if(begin == nums.size() - 1){
             res.push_back(nums);
             return;
         }
         for(int i = begin; i < nums.size(); i++){
-            swap(nums[i],nums[begin]);
-            backtrack(begin + 1,nums);
-            swap(nums[i],nums[begin]);
+            swap(nums[begin],nums[i]);
+            dfs(nums,begin + 1);
+            swap(nums[begin],nums[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        backtrack(0,nums);
+        dfs(nums,0);
         return res;
     }
 };
