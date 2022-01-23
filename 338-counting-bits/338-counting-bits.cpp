1e09fcb1bfbb5c4403bb129b12d6d448a057f2d9
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-01-23 23:17:28 +0530

    Time: 11 ms (23.28%), Space: 8 MB (45.10%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -6,13 +6,13 @@ public:
             return {0};
         dp[0] = 0;
         dp[1] = 1;
-        for(int i = 0; i <= n; i++){
+        for(int i = 2; i <= n; i++){
             if(floor(log2(i)) == log2(i) and i != 0){
                 dp[i] = 1;
             }
             else {
                 int x = pow(2,floor(log2(i)));
-                dp[i] = dp[x] + dp[i-x];
+                dp[i] = dp[i-x] + 1;
             }
         }
         return dp;
