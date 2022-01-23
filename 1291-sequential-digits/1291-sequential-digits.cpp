304bdf88169b906885d0900a71f7381fe61083d1
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-01-23 17:07:29 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (36.79%) - LeetHub

--- 1291-sequential-digits/1291-sequential-digits.cpp
+++ 1291-sequential-digits/1291-sequential-digits.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     vector<int> sequentialDigits(int low, int high) {
-        vector<int> starts = {12,23,34,45,56,67,78,89};
-        vector<int> difs = {11,21,31,41,51,61,71,81};
-        vector<int> nums = starts;
-        vector<int> res;
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < starts.size() - i - 1; j++){
-                difs[j] = difs[j] * 10 + 1;
-                starts[j] += difs[j];
-                nums.push_back(starts[j]);
+        vector<int> nums,res;
+        for(int i = 1; i <= 8; i++){
+            int num = i;
+            int dif = i * 10 + 1;
+            for(int j = 0; j < 9 - i; j++){
+                num += dif;
+                nums.push_back(num);
+                dif = dif * 10 + 1;
             }
         }
+        sort(nums.begin(),nums.end());
         for(int i : nums){
             if(low <= i and i <= high){
                 res.push_back(i);
