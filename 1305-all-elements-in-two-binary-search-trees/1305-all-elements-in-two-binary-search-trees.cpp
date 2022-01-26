4e71aaa394c725ef3828f8d86ffa468b73524d1c
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-01-26 21:34:37 +0530

    Time: 287 ms (12.25%), Space: 84.3 MB (91.67%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.cpp
@@ -19,25 +19,10 @@ public:
         dfs(root->right,nums);
     }
     vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
-        vector<int> nums1,nums2,res;
-        dfs(root1,nums1);
-        dfs(root2,nums2);
-        int i = 0,j = 0;
-        while(i < nums1.size() and j < nums2.size()){
-            if(nums1[i] < nums2[j]){
-                res.push_back(nums1[i]);
-                i++;
-            } else {
-                res.push_back(nums2[j]);
-                j++;
-            }
-        }
-        while(i < nums1.size()){
-            res.push_back(nums1[i++]);
-        }
-        while(j < nums2.size()){
-            res.push_back(nums2[j++]);
-        }
-        return res;
+        vector<int> nums,res;
+        dfs(root1,nums);
+        dfs(root2,nums);
+        sort(nums.begin(),nums.end());
+        return nums;
     }
 };
