f71f357b193cdf5e650ce48cca2ae1a9dd2b8cb2
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-02-01 16:39:48 +0530

    Time: 900 ms (19.36%), Space: 7.9 MB (64.64%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-     bool adjacentSearch(vector<vector<char>>& board, const string& word, int i, int j, int index)
-    {
-        if(index == word.size()) return true; // end condition
-        if(i < 0 || j < 0 || i > board.size()-1 || j > board[0].size()-1) return false; // boundary of matrix
-        if(board[i][j] != word[index]) return false; // do not match
-        // match!
-        board[i][j] = '*'; // change the content, to avoid duplicated search
-        bool furtherSearch =  adjacentSearch(board, word, i+1, j, index+1) || // up
-                              adjacentSearch(board, word, i-1, j, index+1) || // down
-                              adjacentSearch(board, word, i, j-1, index+1) || // left
-                              adjacentSearch(board, word, i, j+1, index+1);   // right
-        
-        board[i][j] = word[index]; // modify it back!
-        return furtherSearch;
-    }
     bool dfs(vector<vector<char>>& board,string word,int i,int j,int pos){
         if(pos == word.size())
             return true;
