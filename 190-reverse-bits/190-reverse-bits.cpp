a6c72f5e4bd6fc01b0e2f4db1d7d30e980c468e4
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-02-08 07:51:40 +0530

    Time: 7 ms (14.49%), Space: 6 MB (51.75%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.cpp
+++ 190-reverse-bits/190-reverse-bits.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        vector<int> bits(32,0);
-        int i = 0;
-        while(n){
-            bits[i++] = n % 2;
-            n /= 2;
-        }
         uint32_t res = 0;
-        for(int i : bits){
-            res = res * 2 + i;
+        for(int i = 0; i <= 31; i++){
+            int bit = n >> i & 1;
+            res = res * 2 + bit;
         }
         return res;
     }
