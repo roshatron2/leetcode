aee2f85cc2397bf5c321b4f2b08d9ec2a705057f
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-02-13 14:23:58 +0530

    Time: 3 ms (69.85%), Space: 7.1 MB (67.91%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res = {{}};
-        for(int i : nums){
+        for(int num : nums){
             int size = res.size();
-            for(int j = 0; j < size; j++){
-                vector<int> cur = res[j];
-                cur.push_back(i);
+            for(int i = 0; i < size; i++){
+                vector<int> cur = res[i];
+                cur.push_back(num);
                 res.push_back(cur);
             }
         }
