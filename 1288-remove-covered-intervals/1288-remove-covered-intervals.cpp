0df47e4722c86eaf38d6938b63c978c07b121ed0
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-02-20 16:16:17 +0530

    Time: 20 ms (95.61%), Space: 11.2 MB (89.47%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int removeCoveredIntervals(vector<vector<int>>& intervals) {
         int covered = 0;
-        sort(intervals.begin(),intervals.end(),[](vector<int> a,vector<int> b){
+        sort(intervals.begin(),intervals.end(),[](vector<int> &a,vector<int> &b){
             if(a[0] == b[0])
                 return a[1] > b[1];
             return a[0] < b[0];
         });
-        int mx = INT_MIN;
-        for(auto interval : intervals){
-            if(interval[1] <= mx)
+        int farthest_endpoint = INT_MIN;
+        for(auto &interval : intervals){
+            if(interval[1] <= farthest_endpoint)
                 covered++;
-            mx = max(interval[1],mx);
+            farthest_endpoint = max(interval[1],farthest_endpoint);
         }
         return intervals.size() - covered;
     }
