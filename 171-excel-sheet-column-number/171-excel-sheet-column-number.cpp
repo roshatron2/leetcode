cede4186806aaaaaa76bad936937a466d92ef00c
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-02-22 21:43:46 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (99.36%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    long long dfs(string s){
-        if(s.size() == 1){
-            return s[0] - 'A' + 1;
+    long long dfs(int n,string &s){
+        if(n == 1){
+            return s[n-1] - 'A' + 1;
         }
-        return 26 * dfs(s.substr(0,s.size() - 1)) + s[s.size() - 1] - 'A' + 1;
-    }    
+        return 26 * dfs(n-1,s) + s[n - 1] - 'A' + 1;
+    }
     int titleToNumber(string s) {
-        return dfs(s);
+        int n = s.size();
+        return dfs(n,s);
     }
 };
