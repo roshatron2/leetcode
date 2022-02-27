258ebb3c156ebd80db06baf8c20d95799bff67a9
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-02-27 17:24:43 +0530

    Time: 13 ms (57.93%), Space: 7.7 MB (87.89%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,36 +1,17 @@
 class Solution {
 public:
-    unordered_map<int,bool> map;
-    bool dfs(string s,vector<string> words,int i){
-        if(map.find(i) != map.end())
-            return map[i];
-        if(i == s.size()){
-            return true;
-        }
-        for(string word : words){
-            int j = 0;
-            for(; j < word.size(); j++){
-                if(s[i + j] != word[j])
-                    break;
-            }
-            if(j == word.size()){
-                return map[i] = dfs(s,words,i + j + 1);
-            }
-        }
-        return map[i] = false;    
-    }
     bool wordBreak(string s, vector<string>& wordDict) {
-        vector<bool> dp(s.size() + 1,false);
+        vector<bool> dp(s.size() + 1);
         dp[0] = true;
         for(int i = 0; i < s.size(); i++){
             if(dp[i]){
                 for(string word : wordDict){
                     if(s.substr(i,word.size()) == word){
                         dp[i + word.size()] = true;
-                    }
+                    }                
                 }
-            }
+            }                
         }
-        return dp[s.size()];
+        return dp[s.size()];                       
     }
 };
