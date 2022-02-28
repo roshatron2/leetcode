aac18333535bbfecf81d22e75a5055d48533c8a8
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-02-28 22:55:24 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (76.11%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    unordered_map<int,int> map;
-    int dfs(vector<int> nums,int pos){
-        if(map.find(pos) != map.end()){
-            return map[pos];
-        }
-        if(pos >= nums.size()){
-            return 0;
-        }
-        map[pos] = max(dfs(nums,pos + 1),dfs(nums,pos + 2) + nums[pos]);
-        return map[pos];
-    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),0);
         if(nums.size() == 1)
             return nums[0];
-        dp[0] = nums[0];
-        dp[1] = max(nums[1],nums[0]);
+        int pre = nums[0],cur = max(nums[0],nums[1]);
         for(int i = 2; i < nums.size(); i++){
-            dp[i] = max(dp[i-1],dp[i-2] + nums[i]);
+            int tmp = cur;
+            cur = max(pre + nums[i],cur);
+            pre = tmp;
         }
-        return dp[nums.size() - 1];
+        return cur;
     }
 };
 
