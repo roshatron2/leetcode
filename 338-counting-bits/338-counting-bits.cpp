1991936310a256f1def9b53fcc3e67abdc8fee9d
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-03-01 12:04:34 +0530

    Time: 10 ms (37.25%), Space: 8 MB (48.88%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int> dp(n + 1,0);
-        if(n == 0) 
-            return {0};
+        if(n == 0)
+            return dp;
         dp[0] = 0;
         dp[1] = 1;
         for(int i = 2; i <= n; i++){
-            if(floor(log2(i)) == log2(i) and i != 0){
+            int x = pow(2,floor(log2(i)));
+            if(x == i)
                 dp[i] = 1;
-            }
-            else {
-                int x = pow(2,floor(log2(i)));
-                dp[i] = dp[i-x] + 1;
-            }
+             else 
+                dp[i] = 1 + dp[i - x];
         }
         return dp;
     }
 };
+
+ 
+    
