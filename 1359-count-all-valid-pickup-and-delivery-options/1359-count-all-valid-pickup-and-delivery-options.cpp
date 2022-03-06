e6042b63964c976bc691f308b47aacfc1661e7a4
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-03-06 21:42:36 +0530

    Time: 79 ms (11.32%), Space: 19.7 MB (12.89%) - LeetHub

---
+++ 1359-count-all-valid-pickup-and-delivery-options/1359-count-all-valid-pickup-and-delivery-options.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int ans = 0;
+    int mod = 1e9 + 7;
+    vector<vector<int>> memo;
+    long totalWays(int unpicked,int undelivered){
+        if(unpicked == 0 and undelivered == 0)
+            return 1;
+        if(unpicked < 0 or undelivered < 0 or undelivered < unpicked){
+            return 0;
+        }
+        if(memo[unpicked][undelivered])
+            return memo[unpicked][undelivered];
+        long ans = 0;
+        ans += unpicked * totalWays(unpicked - 1,undelivered);
+        ans %= mod;
+        ans += (undelivered - unpicked) * totalWays(unpicked,undelivered - 1);
+        ans %= mod;
+        return memo[unpicked][undelivered] = ans;
+    }
+    int countOrders(int n) {
+        memo = vector<vector<int>> (n + 1, vector<int>(n + 1, 0));
+        return totalWays(n,n);
+    }
+};
