2cf846a135ba3b12d9ec74957816ca46f0a1977a
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-03-12 11:21:00 +0530

    Time: 180 ms (40.29%), Space: 15 MB (22.26%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,26 +1,13 @@
 class Solution {
 public:
-    bool binarySearch(vector<vector<int>> &matrix,int start,int target,bool isVertical){
-        int l = start;
-        int r = isVertical ? matrix.size() - 1 : matrix[0].size() - 1;
-        while(l <= r){
-            int mid = l + (r - l) / 2;
-            if(isVertical ? matrix[mid][start] == target : matrix[start][mid] == target){
-                return true;
-            } else if(isVertical ? target < matrix[mid][start] : target < matrix[start][mid]){
-                r = mid - 1;
-            } else {
-                l = mid + 1;
-            }
-        }
-        return false;
-    }
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int shorterDim = min(matrix.size(),matrix[0].size());
-        for(int i = 0; i < shorterDim; i++){
-            bool verticalFound = binarySearch(matrix,i,target,true);
-            bool horizontalFound = binarySearch(matrix,i,target,false);
-            if(verticalFound or horizontalFound){
+        int i = matrix.size() - 1,j = 0;
+        while(i >= 0 and j < matrix[0].size()){
+            if(target < matrix[i][j]){
+                i--;
+            } else if(target > matrix[i][j]){
+                j++;
+            } else {
                 return true;
             }
         }
