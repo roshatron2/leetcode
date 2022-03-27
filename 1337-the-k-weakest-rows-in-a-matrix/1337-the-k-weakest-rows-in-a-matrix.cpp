85ef10b0d0e6b73aeaf90930cc562eda5624b4c6
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-03-27 11:57:17 +0530

    Time: 4 ms (99.88%), Space: 10.4 MB (91.37%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        map<int,vector<int>> map;
+        int n = mat[0].size();
+        auto cmp = [](pair<int,int> a,pair<int,int> b){
+            if(a.first == b.first)
+                return a.second > b.second;
+            return a.first > b.first;
+        };
+        priority_queue<pair<int,int>,vector<pair<int,int>>,decltype(cmp)> pq(cmp);
         for(int i = 0; i < mat.size(); i++){
-            int l = 0,r = mat[0].size() - 1,pos = r + 1,mid;
+            int l = 0,r = n - 1,strength = n,mid;
             
             while(l <= r){
                 mid = l + (r - l) / 2;
                 if(mat[i][mid]){
                     l = mid + 1;
                 } else {
-                    pos = min(pos,mid);
+                    strength = min(strength,mid);
                     r = mid - 1;
                 }
             }
-            map[pos].push_back(i);
+            pq.push({strength,i});
         }
         vector<int> res;
-        for(auto i : map){
-            for(auto j : map[i.first])
-                res.push_back(j);
+        while(!pq.empty() and k){
+            res.push_back(pq.top().second);
+            pq.pop();
+            k--;
         }
-        return vector<int>(res.begin(),res.begin() + k);
+        return res;
     }
 };
