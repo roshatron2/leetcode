dd106e1529af02483e77b0d19ae663d61c3669f7
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-04-01 23:17:53 +0530

    Time: 13 ms (70.60%), Space: 15.6 MB (39.26%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-        int l = 0,r = numbers.size() - 1;
+    int search(vector<int> &nums,int l,int r,int target){
         while(l <= r){
-            int sum = numbers[l] + numbers[r];
-            if(sum < target){
-                l++;
-            } else if(sum > target){
-                r--;
+            int mid = l + (r - l) / 2;
+            if(target < nums[mid]){
+                r = mid - 1;
+            } else if(target > nums[mid]){
+                l = mid + 1;
             } else {
-                return {l + 1,r + 1};
+                return mid;
             }
         }
-        return {};
+        return -1;
+    }
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int> res;
+        int found;
+        for(int i = 0; i < numbers.size(); i++){
+            int cmp = target - numbers[i];
+            if(cmp < numbers[i]){
+                found = search(numbers,0,i - 1,cmp);
+                if(found >= 0){
+                    res = {i + 1,found + 1};
+                    break;
+                }
+            } else {
+                found = search(numbers,i + 1,numbers.size() - 1,cmp);
+                if(found >= 0){
+                    res = {i + 1,found + 1};
+                    break;
+                }
+            }
+        }
+        return res;
     }
 };
