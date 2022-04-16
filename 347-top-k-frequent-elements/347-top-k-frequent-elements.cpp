fc53d5acc6f7e7ef43016d17d184bc643ad384a2
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-04-16 19:54:26 +0530

    Time: 21 ms (53.68%), Space: 13.8 MB (20.89%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -1,24 +1,46 @@
 class Solution {
 public:
-    vector<int> topKFrequent(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<vector<int>> buckets(n + 1);
-        unordered_map<int,int> map;
-        for(int i : nums){
-            map[i]++;
-        }
-        for(auto [key,value] : map){
-            buckets[value].push_back(key);
-        }
-        vector<int> res;
-        for(int i = n; i >= 0 and k > 0; i--){
-            if(!buckets[i].empty()){
-                for(int ele : buckets[i]){
-                    res.push_back(ele);
-                    k--;
-                }
+    vector<int> unique;
+    map<int,int> count;
+    int partition(int left,int right,int pivot_index){
+        int pivot_frequency = count[unique[pivot_index]];
+        swap(unique[pivot_index],unique[right]);
+        
+        int store_index = left;
+        for(int i = left; i <= right; i++){
+            if(count[unique[i]] < pivot_frequency){
+                swap(unique[store_index],unique[i]);
+                store_index++;
             }
         }
+        swap(unique[right],unique[store_index]);
+        return store_index;
+    }
+    void quickSelect(int left,int right,int k_smallest){
+       if(left == right)
+           return;
+        int pivot_index = left + rand() % (right - left + 1);
+        pivot_index = partition(left,right,pivot_index);
+        
+        if(k_smallest == pivot_index){
+            return;
+        } else if(k_smallest < pivot_index){
+            quickSelect(left,pivot_index - 1,k_smallest);
+        } else{
+            quickSelect(pivot_index + 1,right,k_smallest);
+        }
+    }
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        for(int n : nums){
+            count[n]++;
+        }
+        int n = count.size();
+        for(pair<int,int> p : count){
+            unique.push_back(p.first);
+        }
+        quickSelect(0,n - 1,n - k);
+        vector<int> res(k);
+        copy(unique.begin() + n - k,unique.end(),res.begin());
         return res;
     }
 };
