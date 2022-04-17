dfa90ed962a37a65ec3f011215cbbefca5ec50ba
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-04-17 21:03:08 +0530

    Time: 36 ms (42.12%), Space: 25.1 MB (14.94%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -6,10 +6,8 @@ public:
         left[0] = nums[0];
         right[n - 1] = nums[n - 1];
         for(int i = 1; i < nums.size(); i++){
-           left[i] = nums[i] * left[i - 1]; 
-        }
-        for(int i = n - 2; i >= 0; i--){
-            right[i] = right[i + 1] * nums[i];
+           left[i] = nums[i] * left[i - 1];
+           right[n - i - 1] = nums[n - i - 1] * right[n - i];
         }
         for(int i = 0; i < n; i++){
            if(i == 0){
