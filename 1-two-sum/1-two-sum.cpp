407440393cd5c59da81281ed49871d47c7349115
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-04-23 21:02:39 +0530

    Time: 12 ms (83.63%), Space: 10.6 MB (55.94%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target)
-    {
-        //Key is the number and value is its index in the vector.
-        unordered_map<int, int> hash;
-        vector<int> result;
-        for (int i = 0; i < numbers.size(); i++) {
-            int numberToFind = target - numbers[i];
-
-                //if numberToFind is found in map, return them
-            if (hash.find(numberToFind) != hash.end()) {
-                        //+1 because indices are NOT zero based
-                result.push_back(hash[numberToFind]);
-                result.push_back(i);			
-                return result;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
+        for(int i = 0; i < nums.size(); i++){
+            int complement = target - nums[i];
+            if(map.find(complement) != map.end()){
+                return {i,map[complement]};
             }
-
-                //number was not found. Put it in the map.
-            hash[numbers[i]] = i;
-        } 
-        return result;
+            map[nums[i]] = i;
+        }
+        return {};
     }
 };
-
-//P.S No my code
