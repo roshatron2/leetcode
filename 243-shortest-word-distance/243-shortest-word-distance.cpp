2c204fc40de321f4916f64f3dee7a7f86f9e770b
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-05-06 14:50:48 +0530

    Time: 9 ms (80.30%), Space: 11.6 MB (48.42%) - LeetHub

--- 243-shortest-word-distance/243-shortest-word-distance.cpp
+++ 243-shortest-word-distance/243-shortest-word-distance.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int shortestDistance(vector<string>& wordsDict, string word1, string word2) {
-        unordered_map<string,vector<int>> map;
+        int minDist = INT_MAX;
+        int l = -1,r = -1;
         for(int i = 0; i < wordsDict.size(); i++){
-           if(map.find(wordsDict[i]) == map.end()){
-              map[wordsDict[i]] = {i};
+           if(wordsDict[i] == word1){
+               l = i;
            }
-           else {
-              map[wordsDict[i]].push_back(i); 
+           if(wordsDict[i] == word2){
+               r = i;
            }
+           if(l != -1 and r != -1)
+               minDist = min(minDist,abs(l - r));
         }
-        int dist = INT_MAX;
-        for(int i : map[word1]){
-            for(int j : map[word2]){
-               dist = min(dist,abs(i - j)); 
-            }
-        }
-        return dist;
+        return minDist;
     }
 };
-/*
-practice : [0]
-makes : [1,4]
-perfect: [2]
-coding: [3]
-*/
