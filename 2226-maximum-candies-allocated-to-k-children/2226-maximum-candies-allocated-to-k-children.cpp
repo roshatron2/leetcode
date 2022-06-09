8e8e042817a461ddaebb572a146cafe786f98057
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-06-09 11:34:48 +0530

    Time: 281 ms (50.99%), Space: 84.4 MB (61.08%) - LeetHub

--- 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
+++ 2226-maximum-candies-allocated-to-k-children/2226-maximum-candies-allocated-to-k-children.cpp
@@ -3,7 +3,7 @@ public:
     int maximumCandies(vector<int>& candies, long long k) {
         int mx = *max_element(candies.begin(),candies.end());
         int l = 1,r = mx;
-        int ans = 0;
+        int maxCandies = 0;
         while(l <= r){
             int size = (l + r) >> 1;
             long long int piles = 0;
@@ -11,21 +11,12 @@ public:
                piles += candy / size; 
             }
             if(piles >= k){
-                ans = size;        
+                maxCandies = size;        
                 l = size + 1;
             } else {
                 r = size - 1;
             } 
         }
-        return ans;
+        return maxCandies;
     }
 };
-/*
-5 8 6
-k = 3
-l = 5
-r = 5
-mid = 5
-piles = 3
-ans = 4
-*/
