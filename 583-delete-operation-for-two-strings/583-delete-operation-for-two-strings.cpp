8dd71503e96d263e48ba2a8e0974bd75e3f9d755
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-06-14 13:08:36 +0530

    Time: 12 ms (89.07%), Space: 7.5 MB (71.41%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    string s,t;
-    int dp[501][501] = {0};
-    int dfs(int i,int j){
-       if(dp[i][j])
-           return dp[i][j];
-       if(i == s.size() and j == t.size()){
-           return 0;
-       } 
-       if(i == s.size()){
-           return dfs(i,j + 1) + 1;
-       }
-       if(j == t.size()){
-           return dfs(i + 1,j) + 1;
-       }
-       if(s[i] == t[j]){
-           return dfs(i + 1,j + 1);
-       }
-       return dp[i][j] = min(dfs(i + 1,j),dfs(i,j + 1)) + 1; 
-    }
     int minDistance(string word1, string word2) {
-        this->s = word1;
-        this->t = word2;
-        return dfs(0,0);
+        int m = word1.size(),n = word2.size();
+        int dp[m + 1][n + 1];
+        for(int i = 0; i <= n; i++){
+            dp[0][i] = i;
+        }
+        for(int i = 0; i <= m; i++){
+            dp[i][0] = i;
+        }
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(word1[i - 1] == word2[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = min(dp[i - 1][j],dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[m][n];
     }
 };
