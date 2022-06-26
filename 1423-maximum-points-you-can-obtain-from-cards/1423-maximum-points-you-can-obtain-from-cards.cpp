a38aa7129ef0f29c06a5c948e4bbd40d5a41c270
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-06-27 02:45:47 +0530

    Time: 84 ms (56.04%), Space: 42.3 MB (96.60%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -2,28 +2,24 @@ class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
         int n = cardPoints.size();
-        cardPoints.push_back(0);
         k = n - k;
-        int sum = 0;
-        int minWin = INT_MAX;
-        vector<int> prefix(n + 1,0);
-        for(int i = 0; i <= n; i++){
-            prefix[i] = sum;
-            sum += cardPoints[i]; 
+        int window = accumulate(cardPoints.begin(),cardPoints.begin() + k,0);
+        int total = accumulate(cardPoints.begin(),cardPoints.end(),0);
+        int minWin = window;
+        
+        for(int i = k; i < n; i++){
+            window += cardPoints[i] - cardPoints[i - k];
+            minWin = min(window,minWin);
         }
-        for(int i = k; i <= n; i++){
-           minWin = min(minWin,prefix[i] - prefix[i - k]); 
-        }
-        return sum - minWin;
+        return total - minWin; 
     }
 };
 /*
-0 1 2  3  4  5  6 7
-1 2 3  4  5  6  1
-0 1 3  6 10 15 21 22
-
-1 2 3 4 - 10
-2 3 4 5 - 14
-3 4 5 6 - 18
-4 5 6 1 - 16
+0 1 2 3 4 5 6
+1 2 3 4 5 6 1
+i = 5
+k = 4
+sum = 10 + 5 - 1 = 14
+sum = 14 + 6 - 2 = 18
+sum = 18 + 1 - 3 = 16
 */
