029ee4e0fb0c2fe98ba50ef4e0240e72d9e7cc8d
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-07-04 19:41:21 +0530

    Time: 32 ms (55.29%), Space: 17.7 MB (65.79%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,28 +2,19 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        vector<int> left2right(n,1),right2left(n,1); 
-        
+        vector<int> candies(n,1); 
         for(int i = 1; i < n; i++){
             if(ratings[i] > ratings[i - 1]){
-                left2right[i] = left2right[i - 1] + 1;
+                candies[i] = candies[i - 1] + 1;
             }
-        }
+        } 
         for(int i = n - 2; i >= 0; i--){
             if(ratings[i] > ratings[i + 1]){
-                right2left[i] = right2left[i + 1] + 1;
+                if(candies[i] <= candies[i + 1]){
+                    candies[i] = candies[i + 1] + 1;
+                }
             }
         }
-        for(int i = 0; i < n; i++){
-            left2right[i] = max(left2right[i],right2left[i]);
-        }
-        return accumulate(left2right.begin(),left2right.end(),0);
+        return accumulate(candies.begin(),candies.end(),0);
     }
 };
-/*
-0 1 2
-1,0,2
-1 1 2
-2 1 1
-2 1 2
-*/
