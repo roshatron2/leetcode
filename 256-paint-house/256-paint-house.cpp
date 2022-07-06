547d155de964b5fe3b51e923ae8b824ac9cda831
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-07-06 12:04:04 +0530

    Time: 22 ms (12.41%), Space: 10.5 MB (7.42%) - LeetHub

--- 256-paint-house/256-paint-house.cpp
+++ 256-paint-house/256-paint-house.cpp
@@ -4,17 +4,20 @@ public:
        int n = costs.size();
        vector<vector<int>> dp(n,vector<int>(3,0)); 
        dp[0] = costs[0];
+       vector<int> prev = costs[0]; 
        for(int i = 1; i < n; i++){
+           vector<int> cur(3,0);
            for(int j = 0; j < 3; j++){
                if(j == 0){
-                   dp[i][j] = costs[i][j] + min(dp[i - 1][1],dp[i - 1][2]);
+                   cur[j] = costs[i][j] + min(prev[1],prev[2]);
                } else if(j == 1){
-                   dp[i][j] = costs[i][j] + min(dp[i - 1][0],dp[i - 1][2]);
+                   cur[j] = costs[i][j] + min(prev[0],prev[2]);
                } else {
-                   dp[i][j] = costs[i][j] + min(dp[i - 1][0],dp[i - 1][1]);
+                   cur[j] = costs[i][j] + min(prev[0],prev[1]);
                }
            }
+           prev = cur;
        }
-       return *min_element(dp[n - 1].begin(),dp[n - 1].end());
+       return *min_element(prev.begin(),prev.end());
     }
 };
