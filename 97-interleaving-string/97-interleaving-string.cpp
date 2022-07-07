50495b52ade2c8a809827348930241b2de1260f6
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-07-08 01:26:22 +0530

    Time: 88 ms (13.55%), Space: 36.5 MB (11.25%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     unordered_map<string,bool> memo; 
-    bool dfs(string s,string t,string st,int i,int j,int k){
-        string key = to_string(i) + "." + to_string(j) + "." + to_string(k);
+    bool dfs(string s,string t,string st,int i,int j){
+        string key = to_string(i) + "." + to_string(j);
         if(memo.count(key)){
             return memo[key];
         }
-        if(st.size() == k)
+        if(st.size() == i + j)
             return true;
         bool ans = false;
-        if(i < s.size() and s[i] == st[k]){
-            ans |= dfs(s,t,st,i + 1,j,k + 1);
+        if(i < s.size() and s[i] == st[i + j]){
+            ans |= dfs(s,t,st,i + 1,j);
         }
-        if(j < t.size() and t[j] == st[k]){
-            ans |= dfs(s,t,st,i,j + 1,k + 1);
+        if(j < t.size() and t[j] == st[i + j]){
+            ans |= dfs(s,t,st,i,j + 1);
         } 
         return memo[key] = ans;
     }
     bool isInterleave(string s1, string s2, string s3) {
        if(s1.size() + s2.size() != s3.size())
            return false;
-       return dfs(s1,s2,s3,0,0,0); 
+       return dfs(s1,s2,s3,0,0); 
     }
 };
