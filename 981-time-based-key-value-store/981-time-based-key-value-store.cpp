3b1105e16fa83be05efc09427dde546c14441729
Roshan John <63011948+roshatron2@users.noreply.github.com>
2022-10-07 18:44:31 +0530

    Time: 733 ms (24.58%), Space: 128.2 MB (70.18%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -1,23 +1,28 @@
 class TimeMap {
 public:
-    unordered_map<string,map<int,string>> map;
+    unordered_map<string,vector<pair<int,string>>> map;
     TimeMap() {
         
     }
     
     void set(string key, string value, int timestamp) {
-       map[key][timestamp] = value; 
+        map[key].push_back({timestamp,value});
     }
     
     string get(string key, int timestamp) {
-        if(map[key].find(timestamp) != map[key].end()){
-            return map[key][timestamp];    
-        } 
-        auto it = map[key].upper_bound(timestamp);
-        if(it == map[key].begin()){
-            return "";
-        }
-        return prev(it)->second;
+       int lo = 0,hi = map[key].size() - 1,ans; 
+       while(lo <= hi){
+           int mid = lo + (hi - lo) / 2;
+           if(map[key][mid].first <= timestamp ){
+               lo = mid + 1;
+               ans = mid;
+           } else if(map[key][mid].first > timestamp){
+               hi = mid - 1;
+           } 
+       }
+       if(hi == - 1)
+           return "";
+       return map[key][ans].second;
     }
 };
 
@@ -30,15 +35,11 @@ public:
 /*
 */
 /*
-
+find largest number less than or equal to 4
+0 1 2 3 4 
 1 3 5 9 10
-
-foo -> 
-       4 : bar2
-       1 : bar
-get(5)       
-mid = 3
-0 1
-1 4
+lo = 1
+hi = 1
+mid = 1
 
 */
