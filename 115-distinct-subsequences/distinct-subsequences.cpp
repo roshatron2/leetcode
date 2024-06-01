4b74e5c196d6d0d991f7d37695386dd43130d0e5
Roshan John <63011948+roshatron2@users.noreply.github.com>
2024-06-01 11:27:54 +0530

    Time: 18 ms (62.19%) | Memory: 19.8 MB (60.94%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    int dfs(int i, int j, string s, string t, vector<vector<int>>& dp){
-        if(i == s.size()){
-            if(j == t.size()){
-                return 1;
-            } else {
-                return 0;
+    int numDistinct(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        vector<vector<unsigned int>> dp(m + 1, vector<unsigned int>(n + 1, 0));
+        for(int i = 0; i <= m; i++){
+            dp[i][0] = 1;
+        }
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s[i - 1] == t[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
             }
         }
-        if(j == t.size()){
-            return 1;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(s[i] == t[j]){
-            return dp[i][j] = dfs(i + 1, j + 1, s, t, dp) + dfs(i + 1, j, s, t, dp);
-        }
-        return dp[i][j] = dfs(i + 1, j, s, t, dp);
-    }
-    int numDistinct(string s, string t) {
-       vector<vector<int>> dp(s.size(), vector<int>(t.size(), -1));
-       return dfs(0, 0, s, t, dp); 
+       return dp[m][n]; 
     }
 };
