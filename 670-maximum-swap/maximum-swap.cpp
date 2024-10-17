9075cb5cbc583f14b5466200c7229c6a64bc187a
Roshan John <63011948+roshatron2@users.noreply.github.com>
2024-10-17 21:37:48 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (70.34%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string numStr = to_string(num); 
+        int n = numStr.size();
+        vector<int> maxRightIndex(n, n - 1);
+
+        for(int i = n - 2; i >= 0; i--){
+            maxRightIndex[i] = (numStr[i] > numStr[maxRightIndex[i + 1]]) ? i : maxRightIndex[i + 1];
+        }
+
+        for(int i = 0; i < n; i++){
+            if(numStr[i] < numStr[maxRightIndex[i]]){
+                swap(numStr[i], numStr[maxRightIndex[i]]);
+                break;
+            }
+        }
+        return stoi(numStr);
+    }
+};
