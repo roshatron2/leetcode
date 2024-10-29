2106160ebf11eb49ba1dee677b673c1e813d29d6
Roshan John <63011948+roshatron2@users.noreply.github.com>
2024-10-29 23:22:48 +0530

    Time: 22 ms (52.17%) | Memory: 74.3 MB (57.28%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> dirs = {{-1, 1}, {0, 1}, {1, 1}};
-    int m, n;
-    vector<vector<int>> dp;
-    int dfs(vector<vector<int>>& grid, int i, int j){
-        if(dp[i][j]){
-            return dp[i][j];
-        }
-        if(i == grid.size() - 1 and j == grid[0].size() - 1){
-            return 0;
-        }
-
-        int max_moves = 0;
-        for(vector<int> dir : dirs){
-            int x = i + dir[0];
-            int y = j + dir[1];
-
-            if(x >= 0 and y >= 0 and x < grid.size() and y < grid[0].size() 
-                and grid[x][y] > grid[i][j]){
-                    max_moves = max(max_moves, 1 + dfs(grid, x, y));
-            }
-        }
-        return dp[i][j] = max_moves;
-    }
     int maxMoves(vector<vector<int>>& grid) {
-        this->m = grid.size();
-        this->n = grid[0].size();
-        this->dp.assign(m, vector<int>(n, 0));
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
         int max_moves = 0;
+
         for(int i = 0; i < m; i++){
-            max_moves = max(max_moves, dfs(grid, i, 0));
+            dp[i][0] = 1;
+        }
+
+        for(int j = 1; j < n; j++) {
+            for(int i = 0; i < m; i++){
+
+                if(grid[i][j] > grid[i][j - 1] and dp[i][j - 1] > 0){
+                    dp[i][j] = max(dp[i][j], 1 + dp[i][j - 1]);
+                }
+
+                if(i - 1 >= 0 and grid[i][j] > grid[i - 1][j - 1] and dp[i - 1][j - 1] > 0) {
+                    dp[i][j] = max(dp[i][j], 1 + dp[i - 1][j - 1]);
+                }
+
+                if(i + 1 < m and grid[i][j] > grid[i + 1][j - 1] and dp[i + 1][j - 1] > 0){
+                    dp[i][j] = max(dp[i][j], 1 + dp[i + 1][j - 1]);
+                }
+                max_moves = max(max_moves, dp[i][j] - 1);
+            }
         }
         return max_moves; 
     }
