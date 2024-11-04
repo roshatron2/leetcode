8b085a3678f8b17fe2387bf105332f4a7ec7da61
Roshan John <63011948+roshatron2@users.noreply.github.com>
2024-11-05 00:06:21 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (51.28%) - LeetSync

--- 2580-circular-sentence/circular-sentence.cpp
+++ 2580-circular-sentence/circular-sentence.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     bool isCircularSentence(string sentence) {
-        stringstream ss(sentence);        
-        string word;
-        vector<string> words;
-        while(getline(ss, word, ' ')) {
-            words.push_back(word);
-        }
-
-        for(int i = 0; i < words.size() - 1; i++){
-            if(words[i].back() != words[i + 1][0]) {
-                return false; 
+        for(int i = 0; i < sentence.size(); i++) {
+            if(sentence[i] == ' ') {
+                if(sentence[i - 1] != sentence[i + 1]){
+                    return false;
+                }
             }
-        }
+        }        
 
-        return sentence.back() != sentence[0] ? false : true;
+        return sentence[0] != sentence.back() ? false : true;
     }
 };
